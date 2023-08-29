76db2e84c03b9df0fb25062dfd5da5376ade7b76
Rohit garg <84027181+Rohit-max789@users.noreply.github.com>
2023-08-29 14:37:41 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/rohitdynamic',{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect('mongodb+srv://garg2001rohit:Rohit123@cluster0.p7j7gct.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true});
 
 var db=mongoose.connection;
 db.on('error',console.error.bind(console,'connection error:'));
